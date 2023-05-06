89544d8f89df805b3195bb1e6c79f5a39ccf4197
OnurAlan20 <83537043+OnurAlan20@users.noreply.github.com>
2023-05-06 16:47:57 +0300

    selam

--- app/src/main/java/com/onuralan/bootcamp/MainActivity.kt
+++ app/src/main/java/com/onuralan/bootcamp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.onuralan.bootcamp.screens.BootCampViewModel
 import com.onuralan.bootcamp.screens.LoginScreen
 import com.onuralan.bootcamp.ui.theme.BootcampTheme
 
@@ -18,7 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BootcampTheme {
-                LoginScreen()
+                LoginScreen(bootCampViewModel = BootCampViewModel())
 
             }
         }
